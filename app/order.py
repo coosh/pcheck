3dd208bf2fdc701b58cc3c316bb899093086f297
Robert <rob.postill@nedap.com>
2021-01-19 20:35:46 -0500

    Refactor

---
+++ app/order.py
@@ -0,0 +1,8 @@
+class order:
+  def __init__(self, name, buy, sell, volume, buy_corp, sell_corp):
+    self.name      = name
+    self.buy       = buy
+    self.sell      = sell
+    self.volume    = volume
+    self.buy_corp  = buy_corp
+    self.sell_corp = sell_corp
